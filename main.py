c4c67d2c04600e9073a28da3e54f8daee52155e3
amit14916-art <amit14916@gmail.com>
2026-02-17 20:02:53 +0530

    fix create_engine call

--- main.py
+++ main.py
@@ -56,6 +56,11 @@ if not DATABASE_URL:
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+if "sqlite" in DATABASE_URL:
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
